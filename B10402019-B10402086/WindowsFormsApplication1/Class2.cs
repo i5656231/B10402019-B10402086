59630b101c0ab8f9e1c305b7c3f405f79ecc9f29
b10402086 <33798112+john2635360@users.noreply.github.com>
2017-11-21 21:45:04 +0800

    problem3
    Prime_number

--- B10402019-B10402086/WindowsFormsApplication1/Class2.cs
+++ B10402019-B10402086/WindowsFormsApplication1/Class2.cs
@@ -8,5 +8,13 @@ namespace WindowsFormsApplication1
 {
 	class Class2
 	{
+        public bool Prime_number(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return true;
+            }
+            return false;
+        }
 	}
 }
